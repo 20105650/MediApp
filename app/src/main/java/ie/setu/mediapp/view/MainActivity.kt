4ef2faba5aa8904376de075098bfb5b3430aaf9b
20105650 <20105650@mail.wit.ie>
2024-04-26 12:35:09 +0100

    Add Splash Screen

--- app/src/main/java/ie/setu/mediapp/view/MainActivity.kt
+++ app/src/main/java/ie/setu/mediapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package ie.setu.mediapp
+package ie.setu.mediapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ie.setu.mediapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/ie/setu/mediapp/view/SplashScreen.kt
@@ -0,0 +1,23 @@
+package ie.setu.mediapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ie.setu.mediapp.R
+import android.content.Intent
+import android.os.Handler
+
+
+class SplashScreen : AppCompatActivity() {
+    private val SPLASH_DELAY: Long = 3000 // 3 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            // Start main activity
+            startActivity(Intent(this, MainActivity::class.java))
+            // Close this activity
+            finish()
+        }, SPLASH_DELAY)
+    }
+}
